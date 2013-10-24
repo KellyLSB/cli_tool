2d1235feda183dcc4fa45fd3c0033b47cabc69ec
Kelly Becker <kellylsbkr@gmail.com>
2013-10-24 11:27:41 -0700

    Adding more option features

--- lib/cli_tool/option_parser.rb
+++ lib/cli_tool/option_parser.rb
@@ -25,7 +25,71 @@ module CliTool
         return @@options.uniq unless opts
 
         _create_attrs_(opts)
+        default_options(opts)
         @@options = @@options.concat(_create_gola_(opts)).uniq
+      end
+
+      def default_options(opts = {})
+        @@defaults ||= []
+
+        # If no options were passed then return
+        return @@defaults.uniq.inject({}) { |o,(k,v)| o[k] = v; o } unless opts
+
+        # Set the default options
+        opts.each do |opt, details|
+          next unless details.is_a?(Hash)
+          next unless details[:default]
+          opt = opt.first if opt.is_a?(Array)
+          @@defaults << [optionify(opt), details[:default]]
+        end
+
+        @@defaults = @@defaults.uniq
+      end
+
+      # Parse to correct option set
+      def optionify(option, setter = false)
+        option = "#{option}".gsub(/^[\-]+/, '').gsub(/(-| )/, '_')
+        (setter ? option + '=' : option).to_sym
+      end
+
+      # Handle running options
+      def run(entrypoint = false, *args)
+        if args.last.instance_of?(self)
+          instance = args.pop
+        else
+          instance = new
+        end
+
+        # Option Setter Proc
+        option_setter = Proc.new do |option, value|
+          value = case value
+          when ''
+            true
+          when 'true'
+            true
+          when 'false'
+            false
+          else
+            value
+          end
+
+          puts "Setting @#{optionify(option)} = #{value}"
+          instance.__send__(optionify(option, :set), value)
+        end
+
+        # Set options
+        puts "CliTool... Loading Options..."
+        default_options.each(&option_setter)
+        GetoptLong.new(*options).each(&option_setter)
+        puts ''
+
+        # Handle the entrypoint
+        if entrypoint
+          entrypoint = optionify(entrypoint)
+          instance.__send__(entrypoint, *args)
+        else
+          instance
+        end
       end
 
       private
@@ -41,10 +105,10 @@ module CliTool
           key = [key] unless key.is_a?(Array)
           key.each_with_index do |k, i|
             if i < 1
-              default = k.to_sym
-              attr_accessor(k.to_sym)
+              default = optionify(k)
+              attr_accessor(default)
             else
-              define_method("#{k}=", Proc.new { |x| send(default, x) })
+              define_method(optionify(k, :set), Proc.new { |x| send("#{default}=", x) })
             end
           end
         end
@@ -65,12 +129,12 @@ module CliTool
             short = details[:short]
             dependency = details[:dependency]
           else
-            dependency ||= details
+            dependency = details
           end
 
           # Prepare the GetoptLong Array option
           golao  = []
-          golao << short if short
+          golao << "-#{short}" if short
           golao << GOL_MAP[dependency.to_sym]
 
           # If the option has aliases then create
@@ -78,11 +142,11 @@ module CliTool
           if opt.is_a?(Array)
             opt.each do |key|
               golaot = golao.dup
-              golaot.unshift(key)
+              golaot.unshift("--#{key}")
               gola << golaot
             end
           else
-            golao.unshift(opt)
+            golao.unshift("--#{opt}")
             gola << golao
           end
         end
--- lib/cli_tool/version.rb
+++ lib/cli_tool/version.rb
@@ -1,3 +1,3 @@
 module CliTool
-  VERSION = "0.0.1"
+  VERSION = "0.0.2"
 end
